02df82b74ffaf5ba3d5607a6ece597a71c173b92
Shivamraj-Sharma <sharmashivamraj18@gmail.com>
2024-05-15 11:31:20 +0530

    implemented getters and setters

--- Entity/Product.py
+++ Entity/Product.py
@@ -6,3 +6,39 @@ class Product:
         self.price = price
         self.quantityInStock = quantityInStock
         self.type = type
+
+    def getProductId(self) -> int:
+        return self.productId
+
+    def getProductName(self) -> str:
+        return self.productName
+
+    def getDescription(self) -> str:
+        return self.description
+
+    def getPrice(self) -> float:
+        return self.price
+
+    def getQuantityInStock(self) -> int:
+        return self.quantityInStock
+
+    def getType(self) -> str:
+        return self.type
+
+    def setProductId(self, productId: int):
+        self.productId = productId
+
+    def setProductName(self, productName: str):
+        self.productName = productName
+
+    def setDescription(self, description: str):
+        self.description = description
+
+    def setPrice(self, price: float):
+        self.price = price
+
+    def setQuantityInStock(self, quantityInStock: int):
+        self.quantityInStock = quantityInStock
+
+    def setType(self, type: str):
+        self.type = type
