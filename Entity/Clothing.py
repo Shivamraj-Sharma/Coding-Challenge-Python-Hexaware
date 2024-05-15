27c8722aa790072d3a75d78168f2b9e6fe5b1d5c
Shivamraj-Sharma <sharmashivamraj18@gmail.com>
2024-05-15 11:17:51 +0530

    Entities created

---
+++ Entity/Clothing.py
@@ -0,0 +1,7 @@
+from Product import Product
+
+class Clothing(Product):
+    def __init__(self, productId, productName, description, price, quantityInStock, type, size, color):
+        super().__init__(productId, productName, description, price, quantityInStock, type)
+        self.size = size
+        self.color = color
---
+++ Entity/Electronics.py
@@ -0,0 +1,7 @@
+from Product import Product
+
+class Electronics(Product):
+    def __init__(self, productId, productName, description, price, quantityInStock, type, brand, warrantyPeriod):
+        super().__init__(productId, productName, description, price, quantityInStock, type)
+        self.brand = brand
+        self.warrantyPeriod = warrantyPeriod
